4d86d22a82bf8895d4d1d2d8f67f83ae445c629c
UnicornDad8 <unicorndad8@gmail.com>
2023-06-23 18:43:01 -0400

    Annotating exercises

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render
-from django.db.models import Value, F, ExpressionWrapper, DecimalField
-from store.models import Product
+from django.db.models import Value, Max, F, Count, Sum, ExpressionWrapper, DecimalField
+from store.models import Product, Customer, Collection
 
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(F("unit_price") * 0.8, output_field=DecimalField())
+    # Annotating exercises:
+
+    # Customers and their las order id
+    """
+    queryset = Customer.objects.annotate(
+        last_order_id=Max("order__id")
+    )
+    """
+
+    # Collections and count of their products
+    # queryset = Collection.objects.annotate(products_count=Count("product"))
+
+    # Customers with more than 5 orders
+    """
+    queryset = Customer.objects.annotate(
+        orders_count=Count("order")
+    ).filter(orders_count__gt=5)
+    """
+
+    # Customers and the total ammount they've spent
+    """
+    queryset = Customer.objects.annotate(
+        total_spent=Sum(
+            F("order__orderitem__unit_price") *
+            F("order__orderitem__quantity"))
+    )
+    """
+
+    # Top 5 best-selling products and their total sales
     queryset = Product.objects.annotate(
-        discounted_price=discounted_price)
+        total_sales=Sum(
+            F("orderitem__unit_price") *
+            F("orderitem__quantity"))
+    ).order_by("-total_sales")[:5]
 
     return render(request, "hello.html", {"queryset": list(queryset)})
