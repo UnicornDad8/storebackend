b1697d1c27c2e38e72df6d6d1911e6e24666ac5c
UnicornDad8 <unicorndad8@gmail.com>
2023-06-23 18:59:09 -0400

    Quering generic relationships

--- playground/views.py
+++ playground/views.py
@@ -1,42 +1,14 @@
 from django.shortcuts import render
-from django.db.models import Value, Max, F, Count, Sum, ExpressionWrapper, DecimalField
-from store.models import Product, Customer, Collection
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    # Annotating exercises:
-
-    # Customers and their las order id
-    """
-    queryset = Customer.objects.annotate(
-        last_order_id=Max("order__id")
+    content_type = ContentType.objects.get_for_model(Product)
+    queryset = TaggedItem.objects.select_related("tag").filter(
+        content_type=content_type,
+        object_id=1
     )
-    """
-
-    # Collections and count of their products
-    # queryset = Collection.objects.annotate(products_count=Count("product"))
-
-    # Customers with more than 5 orders
-    """
-    queryset = Customer.objects.annotate(
-        orders_count=Count("order")
-    ).filter(orders_count__gt=5)
-    """
-
-    # Customers and the total ammount they've spent
-    """
-    queryset = Customer.objects.annotate(
-        total_spent=Sum(
-            F("order__orderitem__unit_price") *
-            F("order__orderitem__quantity"))
-    )
-    """
-
-    # Top 5 best-selling products and their total sales
-    queryset = Product.objects.annotate(
-        total_sales=Sum(
-            F("orderitem__unit_price") *
-            F("orderitem__quantity"))
-    ).order_by("-total_sales")[:5]
 
     return render(request, "hello.html", {"queryset": list(queryset)})
