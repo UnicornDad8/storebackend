538e3d8d706c778ec115db608fbb330dd38f80e3
UnicornDad8 <unicorndad8@gmail.com>
2023-06-26 09:02:57 -0400

    How transactions work

--- playground/views.py
+++ playground/views.py
@@ -1,32 +1,20 @@
 from django.shortcuts import render
-from store.models import Product, Collection, Cart, CartItem
-from tags.models import TaggedItem
+from django.db import transaction
+from store.models import Product, Collection, Cart, CartItem, Order, OrderItem
 
 
 def say_hello(request):
-    # Creating a shopping cart with an item
-    """
-    cart = Cart()
-    cart.save()
+    # ...
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
 
-    item1 = CartItem()
-    item1.cart = cart
-    item1.product_id = 1
-    item1.quantity = 1
-    item1.save()
-    """
-
-    # Updating the quantity of an item
-    """
-    item1 = CartItem.objects.get(pk=1)
-    item1.quantity = 2
-    item1.save()
-    """
-
-    # Removing cart
-    """
-    cart = Cart(pk=1)
-    cart.delete()
-    """
+        item = OrderItem()
+        item.order = order
+        item.product_id = -1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
 
     return render(request, "hello.html")
