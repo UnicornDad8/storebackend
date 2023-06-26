0d594df407e7f0befcc214975d99fbcf8bc9acde
UnicornDad8 <unicorndad8@gmail.com>
2023-06-26 08:25:18 -0400

    This is a way to update objects

--- playground/views.py
+++ playground/views.py
@@ -4,9 +4,11 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = "Video Games"
-    collection.featured_product = Product(pk=1)
+    """
+    collection = Collection.objects.get(pk=11)
+    collection.featured_product = None
     collection.save()
+    """
+    Collection.objects.filter(pk=11).update(featured_product=None)
 
     return render(request, "hello.html")
